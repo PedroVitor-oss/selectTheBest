d9e29d3b8e91050a47cfd7c5e40011d2e237f6ff
PedroVitor-oss <pedro.santos22@escola.pr.edu.br>
2023-08-05 18:36:01 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,24 +1,78 @@
-const { app } = require("./src/app");
+const { app,server,io } = require("./src/app");
 const { createHome,createStyleHome } = require("./components/home");
-const { port } = require("./config.json");
+const { port,colors,nomes,colorsForUser } = require("./config.json");
+let infoServer = {
+    conected:0,
+    peoples:[],
+    mensagens:[],
+    pickes:[],
+    conparation:[],
+}
 
 app.get("/",(req,res)=>{
 
     const isMobile = req.headers['user-agent'].includes("Mobile");
     
-
+    let palet = createPaletColor(colors.dark);
     res.render("home",
     {
-        title:"app node hbs",
+        title:"ifode - home",
         isMobile,
-        Home:createHome("Seja Bem Vindo","abra o index.js para começar aprogramar seu projeto"),
         htmlStyles:[
-            {css:createStyleHome(isMobile)},
+            {css:palet}
         ],
-        stylesMoblile:[
-           
+        scripts:[
         ]
     })
 })
 
-app.listen(port,console.log("aberto  em https://localhost:"+port));
+io.on('connection', socket=>{
+    //devinir informação do usuario
+    socket.me = {
+        id:socket.id,
+        nome:nomes[Math.round(Math.random() * 100)%nomes.length]+socket.id.substring(0,4),
+        color:colorsForUser[Math.round(Math.random() * 100)%colorsForUser.length],
+    }
+    
+    //nova pessoa conectada
+    
+    infoServer.peoples.push(socket.me);
+    infoServer.conected = infoServer.peoples.length;
+    console.log("nova pessoa conectada - "+ infoServer.conected+" pessoa conectadas")
+    io.emit('dataServer', {infoServer,me:socket.me});
+    console.log(infoServer);
+ 
+
+    //escrever mensagem
+    socket.on('writeMensagem',data =>{
+        infoServer.mensagens.push(data);
+        console.log("nova mensagem - ",data);
+        io.emit('novaMensagem', data);
+    });
+
+
+    //pessoa desconecta 
+    socket.on('disconnect', () =>{
+        for (let i = 0; i < infoServer.peoples.length; i++) {
+            if (infoServer.peoples[i].nome === socket.me.nome) {
+                infoServer.peoples.splice(i, 1);
+                infoServer.conected = infoServer.peoples.length;
+                io.emit('dataServer', {infoServer,me:socket.me});
+                console.log("pessoa desconectada - "+ infoServer.conected+" pessoa conectadas")
+            }
+        }
+        // infoServer.peoples.splice(getForId(socket.id,infoServer).index);
+    })
+})
+
+server.listen(port,console.log("aberto  em https://localhost:"+port));
+
+function createPaletColor(teme){
+    let htmlReturn = `*{\n`;
+    for(color in teme)
+    {
+        htmlReturn+=`--${color}:${teme[color]};\n`;
+    }
+    htmlReturn+=`}`;
+    return(htmlReturn);
+}
--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const { port } = require('../config.json')
 const path = require("path")
 app.use(express.static(path.join(__dirname,'public')));
-// console.log(__dirname)
+
+
+//configuração do socket.io
 
 // configurar handlebars
 const handlebars = require('express-handlebars');
@@ -19,5 +23,7 @@ app.use(bodyParser.json());
 
 module.exports = {
     app,
+    io,
+    server
 }
 
